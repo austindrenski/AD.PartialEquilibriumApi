81fd82bb4cf251956258aec73a45579b5bdcc060
austin.drenski <austin.drenski@gmail.com>
2017-02-02 17:02:18 -0500

    Setting up basic structures

--- AD.PartialEquilibriumApi/Properties/AssemblyInfo.cs
+++ AD.PartialEquilibriumApi/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AD.PartialEquilibriumApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# framework for programming Partial Equilibrium models.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Austin Drenski")]
 [assembly: AssemblyProduct("AD.PartialEquilibriumApi")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
---
+++ AD.PartialEquilibriumApi/src/Model.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace AD.PartialEquilibriumApi
+{
+    /// <summary>
+    /// An abstract base class for partial equilibrium models.
+    /// </summary>
+    [PublicAPI]
+    public abstract class Model
+    {
+    }
+}
