96ebbd233d94da0201e8641189afedd55bb7cdb3
austin.drenski <austin.drenski@gmail.com>
2017-02-02 17:35:56 -0500

    commit

--- AD.PartialEquilibriumApi/src/Model.cs
+++ AD.PartialEquilibriumApi/src/Model.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AD.IO;
+using JetBrains.Annotations;
 
 namespace AD.PartialEquilibriumApi
 {
@@ -8,5 +9,38 @@ namespace AD.PartialEquilibriumApi
     [PublicAPI]
     public abstract class Model
     {
+        /// <summary>
+        /// The number of markets in the model.
+        /// </summary>
+        public int NumberOfMarkets { get; set; }
+
+        /// <summary>
+        /// The number of variables that change during the solution process.
+        /// </summary>
+        public int NumberOfVariables { get; set; }
+
+        /// <summary>
+        /// The user-supplied data for the model.
+        /// </summary>
+        public double[][] Data { get; set; }
+
+        /// <summary>
+        /// Initializes the model.
+        /// </summary>
+        protected Model()
+        {
+            NumberOfMarkets = 0;
+            NumberOfVariables = 0;
+            Data = new double[0][];
+        }
+
+        /// <summary>
+        /// Initializes the model.
+        /// </summary>
+        /// <param name="delimitedFilePath"></param>
+        protected Model(DelimitedFilePath delimitedFilePath)
+        {
+            Data = null;
+        }
     }
 }
