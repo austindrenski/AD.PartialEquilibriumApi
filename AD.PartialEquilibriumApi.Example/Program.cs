673d23444a5bab3553a803d816e81f24dbe7a9db
austin.drenski <austin.drenski@gmail.com>
2017-02-03 13:01:33 -0500

    error in ReadData

--- AD.PartialEquilibriumApi.Example/Program.cs
+++ AD.PartialEquilibriumApi.Example/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Xml.Linq;
+using AD.IO;
 
 namespace AD.PartialEquilibriumApi.Example
 {
@@ -31,37 +35,50 @@ namespace AD.PartialEquilibriumApi.Example
 
         private static XElement ExampleFromXElement()
         {
+            string name = Path.ChangeExtension(Path.GetTempFileName(), ".csv");
+            using (StreamWriter writer = new StreamWriter(name))
+            {
+                writer.WriteLine("ElasticityOfSubstitution,InitialPrice,MarketShare,Tariff");
+                writer.WriteLine("4,0.9764852913975930,0.0164876157540142,0.0435080979193930");
+                writer.WriteLine("4,1.0000000000000000,0.1826886798599640,0.0000000000000000");
+                writer.WriteLine("4,1.0000000000000000,0.0747428059044746,0.0000000000000000");
+                writer.WriteLine("4,1.0000000000000000,0.7260808984815470,0.0000000000000000");
+            }
+            DelimitedFilePath dataFile = DelimitedFilePath.Create(name, ',');
+            IDictionary<string, double[]> data = dataFile.ReadData();
+
+
             // Define the product market.
             XElement usaBeef = new XElement("usaBeef", new XAttribute("ElasticityOfSubstitution", 4));
 
             // Define the supplier markets and the initial prices of purchase from those markets.
             XElement usa =
                 new XElement("usa",
+                    new XAttribute("ElasticityOfSubstitution", 4),
                     new XAttribute("InitialPrice", 0.9764852913975930),
                     new XAttribute("MarketShare",  0.0164876157540142),
-                    new XAttribute("Tariff",       0.0435080979193930),
-                    new XAttribute("ElasticityOfSubstitution", 4));
+                    new XAttribute("Tariff",       0.0435080979193930));
 
             XElement can =
                 new XElement("can",
+                    new XAttribute("ElasticityOfSubstitution", 4),
                     new XAttribute("InitialPrice", 1.0000000000000000),
                     new XAttribute("MarketShare",  0.1826886798599640),
-                    new XAttribute("Tariff",       0.0000000000000000),
-                    new XAttribute("ElasticityOfSubstitution", 4));
+                    new XAttribute("Tariff",       0.0000000000000000));
 
             XElement mex =
                 new XElement("mex",
+                    new XAttribute("ElasticityOfSubstitution", 4),
                     new XAttribute("InitialPrice", 1.0000000000000000),
                     new XAttribute("MarketShare",  0.0747428059044746),
-                    new XAttribute("Tariff",       0.0000000000000000),
-                    new XAttribute("ElasticityOfSubstitution", 4));
+                    new XAttribute("Tariff",       0.0000000000000000));
 
             XElement aus =
                 new XElement("aus",
+                    new XAttribute("ElasticityOfSubstitution", 4),
                     new XAttribute("InitialPrice", 1.0000000000000000),
                     new XAttribute("MarketShare",  0.7260808984815470),
-                    new XAttribute("Tariff",       0.0000000000000000),
-                    new XAttribute("ElasticityOfSubstitution", 4));
+                    new XAttribute("Tariff",       0.0000000000000000));
 
             // Add the supplier markets to the product market.
             // This has the effect of splitting the product market into product supplied by the supplier markets.
