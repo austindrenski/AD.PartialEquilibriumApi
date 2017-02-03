2bcf62e93213532ca463077132d29e2d6a46c3a5
austin.drenski <austin.drenski@gmail.com>
2017-02-03 15:31:51 -0500

    Setting up creation from input files.

--- AD.PartialEquilibriumApi.Example/Program.cs
+++ AD.PartialEquilibriumApi.Example/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 using AD.IO;
 
@@ -11,74 +11,129 @@ namespace AD.PartialEquilibriumApi.Example
     {
         public static void Main()
         {
-            XElement[] versions = new XElement[]
+            string xml = Path.ChangeExtension(Path.GetTempFileName(), ".xml");
+            using (StreamWriter writer = new StreamWriter(xml))
             {
-                ExampleFromXElement(),
-                ExampleFromMarkup()
-            };
-
-            foreach (XElement usaBeef in versions)
-            {
-                Console.WriteLine($"usa initial price:        {usaBeef.Element("usa")?.InitialPrice()}");
-                Console.WriteLine($"can initial price:        {usaBeef.Element("can")?.InitialPrice()}");
-                Console.WriteLine($"mex initial price:        {usaBeef.Element("mex")?.InitialPrice()}");
-                Console.WriteLine($"aus initial price:        {usaBeef.Element("aus")?.InitialPrice()}");
-
-                Console.WriteLine($"usaBeef calculate price index: {usaBeef.CalculatePriceIndex()}");
-
-                Console.WriteLine();
-                Console.WriteLine();
+                writer.WriteLine(
+                    @"<usaBeef >
+                        <usa />
+                        <can />
+                        <mex />
+                        <aus />
+                    </usaBeef>");
             }
-
-            Console.ReadLine();
-        }
-
-        private static XElement ExampleFromXElement()
-        {
-            string name = Path.ChangeExtension(Path.GetTempFileName(), ".csv");
-            using (StreamWriter writer = new StreamWriter(name))
+            XmlFilePath structureFile = new XmlFilePath(xml);
+            
+            string csv = Path.ChangeExtension(Path.GetTempFileName(), ".csv");
+            using (StreamWriter writer = new StreamWriter(csv))
             {
                 writer.WriteLine("ElasticityOfSubstitution,InitialPrice,MarketShare,Tariff");
+                writer.WriteLine("4,1.0000000000000000,0.0000000000000000,0.0000000000000000");
                 writer.WriteLine("4,0.9764852913975930,0.0164876157540142,0.0435080979193930");
                 writer.WriteLine("4,1.0000000000000000,0.1826886798599640,0.0000000000000000");
                 writer.WriteLine("4,1.0000000000000000,0.0747428059044746,0.0000000000000000");
                 writer.WriteLine("4,1.0000000000000000,0.7260808984815470,0.0000000000000000");
             }
-            DelimitedFilePath dataFile = DelimitedFilePath.Create(name, ',');
+            DelimitedFilePath dataFile = new DelimitedFilePath(csv, ',');
+
+            XElement[] versions = new XElement[]
+            {
+                ExampleFromFiles(structureFile, dataFile),
+                ExampleFromXElement(dataFile),
+                ExampleCompleteFromMarkup()
+            };
+
+            foreach (XElement usaBeef in versions)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("Elasticity of substitution:");
+                Console.WriteLine($"    usa: {usaBeef.Element("usa")?.ElasticityOfSubstitution()}");
+                Console.WriteLine($"    can: {usaBeef.Element("can")?.ElasticityOfSubstitution()}");
+                Console.WriteLine($"    mex: {usaBeef.Element("mex")?.ElasticityOfSubstitution()}");
+                Console.WriteLine($"    aus: {usaBeef.Element("aus")?.ElasticityOfSubstitution()}");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Initial price:");
+                Console.WriteLine($"    usa: {usaBeef.Element("usa")?.InitialPrice()}");
+                Console.WriteLine($"    can: {usaBeef.Element("can")?.InitialPrice()}");
+                Console.WriteLine($"    mex: {usaBeef.Element("mex")?.InitialPrice()}");
+                Console.WriteLine($"    aus: {usaBeef.Element("aus")?.InitialPrice()}");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Market share:");
+                Console.WriteLine($"    usa: {usaBeef.Element("usa")?.MarketShare()}");
+                Console.WriteLine($"    can: {usaBeef.Element("can")?.MarketShare()}");
+                Console.WriteLine($"    mex: {usaBeef.Element("mex")?.MarketShare()}");
+                Console.WriteLine($"    aus: {usaBeef.Element("aus")?.MarketShare()}");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Tariff:");
+                Console.WriteLine($"    usa: {usaBeef.Element("usa")?.Tariff()}");
+                Console.WriteLine($"    can: {usaBeef.Element("can")?.Tariff()}");
+                Console.WriteLine($"    mex: {usaBeef.Element("mex")?.Tariff()}");
+                Console.WriteLine($"    aus: {usaBeef.Element("aus")?.Tariff()}");
+
+                Console.WriteLine();
+
+                Console.WriteLine($"usaBeef calculate price index: {usaBeef.CalculatePriceIndex()}");
+
+                Console.WriteLine();
+
+                Console.WriteLine("-------------------------");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static XElement ExampleFromFiles(XmlFilePath structureFile, DelimitedFilePath dataFile)
+        {
+            return XElement.Load(structureFile)
+                           .DefineAttributeData(dataFile);
+        }
+
+        private static XElement ExampleFromXElement(DelimitedFilePath dataFile)
+        {
             IDictionary<string, double[]> data = dataFile.ReadData();
 
-
             // Define the product market.
-            XElement usaBeef = new XElement("usaBeef", new XAttribute("ElasticityOfSubstitution", 4));
+            XElement usaBeef = new XElement("usaBeef",
+                    new XAttribute("ElasticityOfSubstitution", data["ElasticityOfSubstitution"][0]),
+                    new XAttribute("InitialPrice", data["InitialPrice"][0]),
+                    new XAttribute("MarketShare", data["MarketShare"][0]),
+                    new XAttribute("Tariff", data["Tariff"][0]));
 
             // Define the supplier markets and the initial prices of purchase from those markets.
             XElement usa =
                 new XElement("usa",
-                    new XAttribute("ElasticityOfSubstitution", 4),
-                    new XAttribute("InitialPrice", 0.9764852913975930),
-                    new XAttribute("MarketShare",  0.0164876157540142),
-                    new XAttribute("Tariff",       0.0435080979193930));
+                    new XAttribute("ElasticityOfSubstitution", data["ElasticityOfSubstitution"][1]),
+                    new XAttribute("InitialPrice", data["InitialPrice"][1]),
+                    new XAttribute("MarketShare", data["MarketShare"][1]),
+                    new XAttribute("Tariff", data["Tariff"][1]));
 
             XElement can =
                 new XElement("can",
-                    new XAttribute("ElasticityOfSubstitution", 4),
-                    new XAttribute("InitialPrice", 1.0000000000000000),
-                    new XAttribute("MarketShare",  0.1826886798599640),
-                    new XAttribute("Tariff",       0.0000000000000000));
+                    new XAttribute("ElasticityOfSubstitution", data["ElasticityOfSubstitution"][2]),
+                    new XAttribute("InitialPrice", data["InitialPrice"][2]),
+                    new XAttribute("MarketShare", data["MarketShare"][2]),
+                    new XAttribute("Tariff", data["Tariff"][2]));
 
             XElement mex =
                 new XElement("mex",
-                    new XAttribute("ElasticityOfSubstitution", 4),
-                    new XAttribute("InitialPrice", 1.0000000000000000),
-                    new XAttribute("MarketShare",  0.0747428059044746),
-                    new XAttribute("Tariff",       0.0000000000000000));
+                    new XAttribute("ElasticityOfSubstitution", data["ElasticityOfSubstitution"][3]),
+                    new XAttribute("InitialPrice", data["InitialPrice"][3]),
+                    new XAttribute("MarketShare", data["MarketShare"][3]),
+                    new XAttribute("Tariff", data["Tariff"][3]));
 
             XElement aus =
                 new XElement("aus",
-                    new XAttribute("ElasticityOfSubstitution", 4),
-                    new XAttribute("InitialPrice", 1.0000000000000000),
-                    new XAttribute("MarketShare",  0.7260808984815470),
-                    new XAttribute("Tariff",       0.0000000000000000));
+                    new XAttribute("ElasticityOfSubstitution", data["ElasticityOfSubstitution"][4]),
+                    new XAttribute("InitialPrice", data["InitialPrice"][4]),
+                    new XAttribute("MarketShare", data["MarketShare"][4]),
+                    new XAttribute("Tariff", data["Tariff"][4]));
 
             // Add the supplier markets to the product market.
             // This has the effect of splitting the product market into product supplied by the supplier markets.
@@ -87,10 +142,10 @@ namespace AD.PartialEquilibriumApi.Example
             return usaBeef;
         }
 
-        private static XElement ExampleFromMarkup()
+        private static XElement ExampleCompleteFromMarkup()
         {
             return XElement.Parse(
-                @"<usaBeef ElasticityOfSubstitution=""4"">
+                @"<usaBeef ElasticityOfSubstitution=""4"" InitialPrice=""1.0"" MarketShare=""0.0"" Tariff=""0.0"" >
                     <usa ElasticityOfSubstitution=""4"" InitialPrice=""0.9764852913975930"" MarketShare=""0.0164876157540142"" Tariff=""0.0435080979193930"" />
                     <can ElasticityOfSubstitution=""4"" InitialPrice=""1.0000000000000000"" MarketShare=""0.1826886798599640"" Tariff=""0.0000000000000000"" />
                     <mex ElasticityOfSubstitution=""4"" InitialPrice=""1.0000000000000000"" MarketShare=""0.0747428059044746"" Tariff=""0.0000000000000000"" />
---
+++ AD.PartialEquilibriumApi/src/DefineAttributeData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using AD.IO;
+using JetBrains.Annotations;
+
+namespace AD.PartialEquilibriumApi
+{
+    /// <summary>
+    /// Extension methods to set attribute values on the root and descendants using a data file.
+    /// </summary>
+    [PublicAPI]
+    public static class DefineAttributeDataExtensions
+    {
+        /// <summary>
+        /// Defines attributes on the root and descendants using a data file.
+        /// </summary>
+        /// <param name="root">The root element.</param>
+        /// <param name="dataFile">A <see cref="DelimitedFilePath"/> with headers and double values.</param>
+        /// <returns>A reference to the existing <see cref="XElement"/>. This is returned for use with fluent syntax calls.</returns>
+        public static XElement DefineAttributeData(this XElement root, DelimitedFilePath dataFile)
+        {
+            IDictionary<string, double[]> data = dataFile.ReadData();
+
+            XElement[] elements = root.DescendantsAndSelf().ToArray();
+
+            foreach (string header in data.Keys)
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    elements[i].SetAttributeValue(header, data[header][i]);
+                }
+            }
+
+            return root;
+        }
+    }
+}
