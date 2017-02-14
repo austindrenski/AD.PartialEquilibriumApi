97b583027714eeb6737bc925db9387d289e423ce
austin.drenski <austin.drenski@gmail.com>
2017-02-14 13:03:16 -0500

    saved

--- AD.PartialEquilibriumApi/src/Xml/ConsumerPrice.cs
+++ AD.PartialEquilibriumApi/src/Xml/ConsumerPrice.cs
@@ -68,7 +68,6 @@ namespace AD.PartialEquilibriumApi
                 }
                 else
                 {
-                    --index;
                     consumerPrice = consumerPriceIndex;
                 }
 
